4907b4aa96f744cce3136e007b28a861b4f2785a
hikimi0 <hokimthanh8@gmail.com>
2025-04-06 21:51:58 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+    const email = document.querySelector("input[type='email']").value;
+    if (!email.includes("@")) {
+      alert("Please enter a valid email address.");
+      e.preventDefault();
+    }
+  });
+  
