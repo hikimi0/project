7e2ead34c2300679f4b8be7b6a74236e2a689e0d
hikimi0 <hokimthanh8@gmail.com>
2025-06-15 16:30:58 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("form").addEventListener("submit", function (e) {
-    const email = document.querySelector("input[type='email']").value;
-    if (!email.includes("@")) {
-      alert("Please enter a valid email address.");
-      e.preventDefault();
-    }
-  });
-  
